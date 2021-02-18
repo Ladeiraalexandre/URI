f5cf3f626728eee6fe1c24801c8103346d7347dd
Ladeira <ladeiraalexandre@gmail.com>
2021-02-17 22:35:04 -0300

    Adicionado URI_1016

--- INICIANTE/URI_1016.cs
+++ INICIANTE/URI_1016.cs
@@ -1,1 +1,17 @@
 
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int km = int.Parse(Console.ReadLine());
+        double tempo = (60 * km) / 30;
+
+        Console.WriteLine(tempo.ToString() + " minutos");
+
+    }
+
+}
